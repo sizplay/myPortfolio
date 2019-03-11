0bf21ed8ac6d3afaa74eea2adaebdc3ee2791fc2
sizplay@gmail.com <sizplay@gmail.com>
2019-03-11 13:19:42 -0400

    Updates

---
+++ precache-manifest.ed25445263f9d726d190825a349f1827.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d45a4172101e8057a9ee",
+    "url": "/myPortfolio/static/css/main.a81b8e69.chunk.css"
+  },
+  {
+    "revision": "d45a4172101e8057a9ee",
+    "url": "/myPortfolio/static/js/main.7b0733be.chunk.js"
+  },
+  {
+    "revision": "11bd7fd3f5d51cc485df",
+    "url": "/myPortfolio/static/js/runtime~main.59568226.js"
+  },
+  {
+    "revision": "438e10dc2a3f8d95e2d4",
+    "url": "/myPortfolio/static/js/2.3efe83fd.chunk.js"
+  },
+  {
+    "revision": "543cc92bf60e8004bd4c80936b62d960",
+    "url": "/myPortfolio/static/media/PlanIt.543cc92b.png"
+  },
+  {
+    "revision": "f79a1aa9a962258b1d6e80377960c98e",
+    "url": "/myPortfolio/static/media/face.f79a1aa9.png"
+  },
+  {
+    "revision": "74e06430f2b6ddd587ebd1d69db90f66",
+    "url": "/myPortfolio/static/media/movieMarker.74e06430.png"
+  },
+  {
+    "revision": "7acca6eb8bcdfffd8bf7562015e09432",
+    "url": "/myPortfolio/static/media/theLightWeb.7acca6eb.png"
+  },
+  {
+    "revision": "8343c41b57c62c072ea7a1a0f164d3b7",
+    "url": "/myPortfolio/static/media/SAM_2279.8343c41b.JPG"
+  },
+  {
+    "revision": "d9dd668e4bd9ebadb6edeada8cb27485",
+    "url": "/myPortfolio/index.html"
+  }
+];
